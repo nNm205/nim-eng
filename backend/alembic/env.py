a70e7fafc9d7bc95b3095b21af345828523740b8
nNm205 <minh2m5@gmail.com>
2026-05-21 17:37:52 +0700

    chore(db): restructure initial alembic migration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
 from app.database.base import Base 
 from app.config import settings
 import app.models 
@@ -25,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = Base.metadata 
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
