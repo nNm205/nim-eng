a236ee1da499231949b354d2157eafbaf3ada4ab
nNm205 <minh2m5@gmail.com>
2026-05-21 11:42:57 +0700

    chore(app): integrate project routes and database session updates

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from app.utils.logger import logger 
-from app.routes import auth 
-
+from app.routes import auth, projects
 
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(projects.router)
 
 @app.get("/")
 def root():
